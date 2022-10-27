bc14ff05093657c564a19c1eb0997e5a41fc5440
mengdechao <mengdechaolive@qq.com>
2022-10-27 23:51:35 +0800

    add webrtc demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React from 'react';
+import { sdpFilterCodec } from './utils';
+import {Col, Row} from 'antd'
+import 'antd/dist/antd.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.localVideoRef = React.createRef()
+    this.remoteVideoRef = React.createRef()
+    this.state = {}
+    console.log('cons')
+  }
+  componentDidMount() {
+    console.log('mount')
+    this.start()
+  }
+
+  async start() {
+    this.pc = this.createPeerConnection();
+    var pc = this.pc
+    var constraints = {
+      audio: false,
+      video: true
+    }
+    var stream = await navigator.mediaDevices.getUserMedia(constraints)
+    this.localVideoRef.current.srcObject = stream;
+    stream.getTracks().forEach(track => {
+      console.log('add local track')
+      pc.addTrack(track, stream);
+    })
+    try {
+      this.negotiate(pc);
+    } catch (e) {
+      alert('Could not acquire media: ' + e);
+    }
+  }
+
+  async playVideoFromCamera() {
+    try {
+      const constraints = { 'video': true, 'audio': true };
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      this.remoteVideoRef.current.srcObject = stream;
+    } catch (error) {
+      console.error('Error opening video camera.', error);
+    }
+
+  }
+  async negotiate(pc) {
+    var offer = await pc.createOffer()
+    await pc.setLocalDescription(offer);
+    await new Promise(function (resolve) {
+      if (pc.iceGatheringState === 'complete') {
+        resolve();
+      } else {
+        function checkState() {
+          if (pc.iceGatheringState === 'complete') {
+            pc.removeEventListener('icegatheringstatechange', checkState);
+            resolve();
+          }
+        }
+        pc.addEventListener('icegatheringstatechange', checkState);
+      }
+    });
+
+    var codec;
+    var offer = pc.localDescription
+
+    codec = 'default'
+    // offer.sdp = sdpFilterCodec('video', codec, offer.sdp);
+    var response = await fetch('http://localhost:8080/offer', {
+      body: JSON.stringify({
+        sdp: offer.sdp,
+        type: offer.type,
+        video_transform: 'rotate'
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      // mode: 'no-cors'
+    });
+    var answer = await response.json();
+    console.log(answer);
+    await pc.setRemoteDescription(answer);
+  }
+
+  createPeerConnection() {
+    var config = {
+      sdpSemantics: 'unified-plan'
+    };
+
+    var pc = new RTCPeerConnection(config);
+
+    // connect audio / video
+    pc.addEventListener('track', (evt) => {
+      if (evt.track.kind == 'video') {
+        console.log(evt);
+        this.remoteVideoRef.current.srcObject = evt.streams[0];
+      }
+      else
+        ;
+      // document.getElementById('audio').srcObject = evt.streams[0];
+    });
+    return pc;
+  }
+
+  render() {
+    return (
+      <div>
+          <Row>
+            <Col span={8} offset={4}>
+              <video id="localVideo" autoPlay playsInline controls={true} ref={this.localVideoRef}></video>
+            </Col>
+            <Col span={8}>
+              <video id="remoteVideo" autoPlay playsInline controls={true} ref={this.remoteVideoRef}></video>
+            </Col>
+          </Row>
+      </div>
+    );
+
+  }
 }
 
-export default App;
+export default MyApp;
---
+++ src/utils.js
@@ -0,0 +1,60 @@
+export function sdpFilterCodec(kind, codec, realSdp) {
+    var allowed = []
+    var rtxRegex = new RegExp('a=fmtp:(\\d+) apt=(\\d+)\r$');
+    var codecRegex = new RegExp('a=rtpmap:([0-9]+) ' + escapeRegExp(codec))
+    var videoRegex = new RegExp('(m=' + kind + ' .*?)( ([0-9]+))*\\s*$')
+    
+    var lines = realSdp.split('\n');
+
+    var isKind = false;
+    for (var i = 0; i < lines.length; i++) {
+        if (lines[i].startsWith('m=' + kind + ' ')) {
+            isKind = true;
+        } else if (lines[i].startsWith('m=')) {
+            isKind = false;
+        }
+
+        if (isKind) {
+            var match = lines[i].match(codecRegex);
+            if (match) {
+                allowed.push(parseInt(match[1]));
+            }
+
+            match = lines[i].match(rtxRegex);
+            if (match && allowed.includes(parseInt(match[2]))) {
+                allowed.push(parseInt(match[1]));
+            }
+        }
+    }
+
+    var skipRegex = 'a=(fmtp|rtcp-fb|rtpmap):([0-9]+)';
+    var sdp = '';
+
+    isKind = false;
+    for (var i = 0; i < lines.length; i++) {
+        if (lines[i].startsWith('m=' + kind + ' ')) {
+            isKind = true;
+        } else if (lines[i].startsWith('m=')) {
+            isKind = false;
+        }
+
+        if (isKind) {
+            var skipMatch = lines[i].match(skipRegex);
+            if (skipMatch && !allowed.includes(parseInt(skipMatch[2]))) {
+                continue;
+            } else if (lines[i].match(videoRegex)) {
+                sdp += lines[i].replace(videoRegex, '$1 ' + allowed.join(' ')) + '\n';
+            } else {
+                sdp += lines[i] + '\n';
+            }
+        } else {
+            sdp += lines[i] + '\n';
+        }
+    }
+
+    return sdp;
+}
+
+function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
